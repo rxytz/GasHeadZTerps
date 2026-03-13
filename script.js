2c6efaed087c0bde8cf32140647de7debd487fdc
rxytz <sileya6233@niprack.com>
2026-03-13 18:30:44 +0100

    Add info button to homepage

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.getElementById('shop-button').addEventListener('click', () => {
     window.location.href = 'shop.html';
 });
+
+document.getElementById('info-button').addEventListener('click', () => {
+    alert('Willkommen bei GasHeadZ! Hier findest du die neuesten Trends und Styles.');
+});
